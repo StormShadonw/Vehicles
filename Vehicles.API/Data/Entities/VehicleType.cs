e7572c55f57de36a16f62e68e941fab4efce27d2
StormShadonw <luisjorgelopezhernandez@hotmail.com>
2021-11-29 01:14:38 -0400

    First commit

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+        [Display(Name ="Tipo de Vehiculo")]
+        [MaxLength(50, ErrorMessage ="El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        public String Description { get; set; }
+    }
+}
