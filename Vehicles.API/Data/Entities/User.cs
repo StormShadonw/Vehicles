bab16d4ed5bd1ca36dbaacc6958f3f971f908147
StormShadonw <luisjorgelopezhernandez@hotmail.com>
2021-12-04 14:22:59 -0400

    Capitulo 6

---
+++ Vehicles.API/Data/Entities/User.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Data.Entities
+{
+	public class User : IdentityUser
+	{
+		[Display(Name = "Nombres")]
+		[MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+		[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+		public string FirstName { get; set; }
+
+		[Display(Name = "Apellidos")]
+		[MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+		[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+		public string LastName { get; set; }
+
+		[Display(Name = "Tipo de documento")]
+		[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+		public DocumentType DocumentType { get; set; }
+
+		[Display(Name = "Documento")]
+		[MaxLength(20, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+		[Required(ErrorMessage = "El campo {0} es obligatorio.")]
+		public string Document { get; set; }
+
+		[Display(Name = "Dirección")]
+		[MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres.")]
+		public string Address { get; set; }
+
+		[Display(Name = "Foto")]
+		public Guid ImageId { get; set; }
+
+		//TODO: Fix the images path
+		[Display(Name = "Foto")]
+		public string ImageFullPath => ImageId == Guid.Empty 
+			? $"https://vehiclesprep.azurewebsites.net/images/noimage.png" 
+			: $"https://vehicleszuluprep.blob.core.windows.net/users/{ImageId}";
+	}
+}
