995ed2af15c97955583227653f9c49d10a238fbd
StormShadonw <luisjorgelopezhernandez@hotmail.com>
2021-12-07 18:38:14 -0400

    Login and Authorize

--- Vehicles.API/Controllers/VehicleTypesController.cs
+++ Vehicles.API/Controllers/VehicleTypesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
@@ -8,6 +9,7 @@ using Vehicles.API.Data.Entities;
 
 namespace Vehicles.API.Controllers
 {
+	[Authorize(Roles ="Admin")]
 	public class VehicleTypesController : Controller
 	{
 		private readonly DataContext _context;
