51d1463efd46ed96934e5dcb399349f71f75995c
StormShadonw <luisjorgelopezhernandez@hotmail.com>
2022-01-07 11:01:50 -0400

    Images vehicles working!

--- Vehicles.API/Controllers/UsersController.cs
+++ Vehicles.API/Controllers/UsersController.cs
@@ -351,13 +351,18 @@ namespace Vehicles.API.Controllers
 			{
 				return NotFound();
 			}
-
+			try
+			{
 			await _blobHelper.DeleteBlobAsync(vehiclePhoto.ImageId, "vehicles");
+			}
+			catch
+			{
+
+			}
 			_context.VehiclePhotos.Remove(vehiclePhoto);
 			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(EditVehicle), new { id = vehiclePhoto.Id });
+			return RedirectToAction(nameof(EditVehicle), new { id = vehiclePhoto.Vehicle.Id });
 		}
-
 
 		public async Task<IActionResult> AddVehicleImage(int? id)
 		{
@@ -382,9 +387,30 @@ namespace Vehicles.API.Controllers
 			return View(model);
 		}
 
-
-
-
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> AddVehicleImage(VehiclePhotoViewModel model)
+		{
+			if(ModelState.IsValid)
+			{
+				Guid imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "vehicles");
+				Vehicle vehicle = await _context.Vehicles
+					.Include(x => x.VehiclePhotos)
+					.FirstOrDefaultAsync(x => x.Id == model.VehicleId);
+				if(vehicle.VehiclePhotos == null)
+				{
+					vehicle.VehiclePhotos = new List<VehiclePhoto>();
+				}
+				vehicle.VehiclePhotos.Add(new VehiclePhoto
+				{
+					ImageId = imageId
+				});
+				_context.Vehicles.Update(vehicle);
+				await _context.SaveChangesAsync();
+				return RedirectToAction("EditVehicle", new {id = vehicle.Id});
+			}
+			return View(model);
+		}
 
 	}
 
