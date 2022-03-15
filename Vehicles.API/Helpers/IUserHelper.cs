2f1f042f7aa6e89ae07108bbbea9bc998924178e
StormShadonw <luisjorgelopezhernandez@hotmail.com>
2022-03-15 19:19:23 -0400

    Change User and Change User Password.

--- Vehicles.API/Helpers/IUserHelper.cs
+++ Vehicles.API/Helpers/IUserHelper.cs
@@ -22,6 +22,8 @@ namespace Vehicles.API.Helpers
 		Task<SignInResult> LoginAsync(LoginViewModel model);
 		Task LogoutAsync();
 
+		Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
 
 	}
 }
